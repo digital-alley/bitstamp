152e06aef0b32ffa7d9d80731d4fd6e65592cb08
Nejc Goršič <nejcgorsic1@gmail.com>
2021-05-25 20:05:27 +0200

    Refactor make request function. Add HTTP method enums.

--- rest/client.py
+++ rest/client.py
@@ -1,9 +1,15 @@
-import requests
 import typing
+
+import requests
 
 
 class APIV2Client:
-    base_endpoint: str = 'https://www.bitstamp.net/api/v2'
+    _BASE_ENDPOINT: str = 'https://www.bitstamp.net/api/v2'
+    _HTTP_GET: str = 'GET'
+    _HTTP_POST: str = 'POST'
+    _HTTP_DELETE: str = 'DELETE'
+    _HTTP_PATCH: str = 'PATCH'
+    _HTTP_PUT: str = 'PUT'
 
     def __init__(self, client_id: str = None, api_key: str = None, api_secret: str = None):
         pass
@@ -13,14 +19,14 @@ class APIV2Client:
         Executes an HTTP request calling latest ticker info.
         :param currency_pair: defines which currency pair to get ticker for.
         """
-        return self._make_request('GET', '/ticker/' + currency_pair)
+        return self._make_request(self._HTTP_GET, '/ticker/' + currency_pair)
 
     def hourly_ticker(self, currency_pair: str) -> str:
         """
         Executes an HTTP request calling hourly ticker info.
         :param currency_pair: defines which currency pair to get hourly ticker for.
         """
-        return self._make_request('GET', '/ticker_hour/' + currency_pair)
+        return self._make_request(self._HTTP_GET, '/ticker_hour/' + currency_pair)
 
     def order_book(self, currency_pair: str, group: int = 1):
         """
@@ -37,9 +43,9 @@ class APIV2Client:
             raise ValueError('Group parameter should be 0, 1 or 2.')
 
         params = {'group': group}
-        return self._make_request('GET', '/order_book/' + currency_pair, params)
+        return self._make_request(self._HTTP_GET, '/order_book/' + currency_pair, params)
 
-    def transactions(self, currency_pair: str, period: str = "hour"):
+    def transactions(self, currency_pair: str, period: str = 'hour'):
         """
         Returns descending list of transactions for specified currency pair. Supports time interval from which we want the transactions
         to be returned.
@@ -51,28 +57,36 @@ class APIV2Client:
         day - returns transactions for the day.
         """
         params = {'time': period}
-        return self._make_request('GET', '/transactions/' + currency_pair, params)
+        return self._make_request(self._HTTP_GET, '/transactions/' + currency_pair, params)
 
     def conversion_rate(self):
         """
         Retrieves current BUY/SELL conversion rate for EUR/USD.
         """
-        return self._make_request('GET', '/eur_usd/')
+        return self._make_request(self._HTTP_GET, '/eur_usd/')
 
-    def _make_request(self, method: str, endpoint: str, params: typing.Dict = None, body: typing.Dict = None):
+    @classmethod
+    def _make_request(cls, method: str, endpoint: str, params: typing.Dict = None, body: typing.Dict = None):
         """
         Private function for all different HTTP request methods using request library.
 
         :param method: indicates the HTTP request method (GET, POST,...)
         :param endpoint: specific endpoint for for api endpoints.
-        :param data: additional parameters
+        :param params: additional parameters
+        :param body: Body of the request
         :return: json value of response.
         """
-        if method == 'GET':
-            try:
-                rsp = requests.get(url=self.base_endpoint + endpoint, params=params)
-            except Exception as e:
-                raise ValueError('Connection error while making request %s: with endpoint: %s, error: %s', method,
-                                 endpoint, e)
+        try:
+            rsp = {
+                cls._HTTP_GET: requests.get(url=cls._BASE_ENDPOINT + endpoint, params=params),
+                cls._HTTP_POST: requests.get(url=cls._BASE_ENDPOINT + endpoint, params=params)
+            }.get(method)
 
-        return rsp.json()
+            if rsp is None:
+                raise Exception('Unknown HTTP method')
+
+            return rsp.json()
+
+        except Exception as e:
+            raise ValueError('Connection error while making request %s: with endpoint: %s, error: %s', method,
+                             endpoint, e)
